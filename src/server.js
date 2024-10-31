abe3d815512d199751fe5e81df38ff576ee7ddcd
Novoselskyi Feliks <110721352+FeliksNovoselskyi@users.noreply.github.com>
2024-10-31 22:50:11 +0200

    first project page

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import {PrismaClient} from '@prisma/client'
 
 const app = express()
+const prisma = new PrismaClient()
 
 const PORT = 3000
 const HOST = 'localhost'
@@ -9,8 +11,36 @@ const HOST = 'localhost'
 app.set('view engine', 'ejs')
 app.set('views', './templates')
 
+app.use(express.urlencoded({extended: true}))
+
+let context
+
 app.get('/', (req, res) => {
-	res.render('index')
+	context = {}
+
+	context.error = null
+
+	res.render('index', context)
+})
+
+app.post('/',  async (req, res) => {
+	context = {}
+
+	const {name, email} = req.body
+
+	if (!name || !email) {
+		context.error = "Fill all inputs!"
+	} else {
+		const user = await prisma.user.create({
+			data: {
+				name: name,
+				email: email
+			}
+		})
+		context.error = null
+	}
+
+    res.render('index', context)
 })
 
 app.listen(PORT, HOST, () => {
